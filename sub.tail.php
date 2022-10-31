be74be0d012446ae5eadda6eec61744b0b9b9e5b
lee <sipossa@naver.com>
2022-10-31 12:02:21 +0900

    20221031

--- sub.tail.php
+++ sub.tail.php
@@ -4,13 +4,8 @@
             <div class="lnb">
                 <?php include G5_THEME_PATH.'/doc/nav.php' ?>
             </div>  
-            
-            <h3>CONTACT US</h3>
-            <ul class="contact">
-                <li class="tel"><a href="tel:<?=$as_tel?>"><?=$as_tel?></a></li>
-                <li>Fax : <?=$as_fax?></li>
-                <li>E-mail : <a href="mailto:<?=$as_mail?>"><?=$as_mail?></a></li>
-            </ul>
+            <? include_once(G5_THEME_PATH.'/customer.php');?>
+
         </aside> 
         </div>
     </div>
